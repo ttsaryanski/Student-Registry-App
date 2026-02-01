7eda21664de4638b31429d6769289601ab694573
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2026-02-01 10:43:56 +0200

    Edit test

--- tests/home.tests.js
+++ tests/home.tests.js
@@ -1,14 +1,14 @@
-const assert = require('assert');
-const fetch = require('node-fetch');
+const assert = require("assert");
+const fetch = require("node-fetch");
 
-suite('Home page', function() {
-  test('Page title', async function() {
+suite("Home page", function () {
+  test("Page title", async function () {
     let res = await fetch("http://localhost:8888/");
     let body = await res.text();
-    assert.ok(body.includes("<h1>Students Registry</h1>"));
+    assert.ok(body.includes("<h1>Users Registry</h1>"));
   });
-  
-  test('Students count', async function() {
+
+  test("Students count", async function () {
     let res = await fetch("http://localhost:8888/");
     let body = await res.text();
     assert.ok(body.includes("Registered students: <b>2</b>"));
